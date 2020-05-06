738d925f7502ed7a5fa97303f27f6695a22ed182
layscape <57191240+layscape@users.noreply.github.com>
2020-05-06 10:27:00 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+## LOGIN
+Username = 'akumaru40'
+Password = 'aqmar2012'
+##BET SETTINGS
+base_bet = float(0.0010)
+if_lose = int(2)
+chance1 = int(25) ##isi sama jika chance tidak random
+chance2 = int(47) ##isi sama jika chance tidak random
+target_profit = float(1000)
+interval = float(0.3) ##second
