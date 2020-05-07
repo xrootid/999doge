c959e3728465fa7feab74917dd7688fe1a8e9f3f
layscape <57191240+layscape@users.noreply.github.com>
2020-05-07 20:14:30 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,17 @@
 Username = 'xxx'
 Password = 'xxx'
 ##BET SETTINGS
-base_bet = float(0.0010)
-if_lose = float(1.3)
-chance1 = int(9) ##isi sama jika chance tidak random
-chance2 = int(25) ##isi sama jika chance tidak random
-target_profit = float(1000)
-interval = float(0.3) ##second
+base_bet = float(0.010)
+if_lose = float(1.2)
+chance1 = int(7) ##isi sama jika chance tidak random
+chance2 = int(15) ##isi sama jika chance tidak random
+target_profit = float(0.1)
+interval = float(0.1) ##second
+#TOOLS
+#otomatis refresh setelah mencapai target profit
+autorefresh = "ON"#ON/OFF || REFRESH SETIAP PROFIT TERPENUHI
+#otomatis withdraw setelah ifbalance
+autowithdraw = "ON"#ON/OFF
+ifbalance = int(110)
+address = "D8nonR27Twya6ZaivX1uiErkc1c3m1yt3u"
+amount = 10
