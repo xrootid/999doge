80e1f606aaecdae1ddd04c039ca6dc30efd04110
layscape <57191240+layscape@users.noreply.github.com>
2020-06-02 19:57:05 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,26 @@
-## LOGIN
+## Account 999Doge
 Username = 'xxx'
 Password = 'xxx'
-##BET SETTINGS SILAKAN ATUR SENDIRI
-base_bet = float(0.010)
-if_lose = float(1.2)
-chance1 = int(7) ##isi sama jika chance tidak random
-chance2 = int(15) ##isi sama jika chance tidak random
-target_profit = float(0.1)
-interval = float(0.1) ##second
+
+
+##BET SETTINGS
+base_bet = float(0.000001) ##your first bet
+resetif_win = int(1) ##refresh  after (x) win
+resetif_lose = int(1) ##refresh  after (x) lose
+if_win = float(0.3) ##if win
+if_lose = float(3) ##if lose
+chance1 = int(40) ##fill same if chance not random
+chance2 = int(49) ##fill same if chance not random
+target_profit = float(0.2) ##target profit each session
+interval = float(0.1) ##how fast ur bet
+intervalif_lose = float(1) ##interval bet when you lose
+interfalif_win = float(0) ##interval win when you lose
+intervalrefresh = float(2) ##interval refresh when refreshing
 #TOOLS
-#otomatis refresh setelah mencapai target profit
-autorefresh = "ON"#ON/OFF || REFRESH SETIAP PROFIT TERPENUHI
-#otomatis withdraw setelah ifbalance
-autowithdraw = "OFF"#ON/OFF
-ifbalance = int(42)
-address = "DAUBWjznsAeddRw7X19j2ARShMMu62uPKU"
-amount = 10
+
+autorefresh = "ON"#ON/OFF ##automatic refresh when target profit reached
+
+autowithdraw = "OFF"#ON/OFF ##automatic withdraw when balance reached *must trigered by autorefresh*
+ifbalance = int(500)
+address = "DAUBWjznsAeddRw7X19j2ARShMMu62uPKU" ##change to your wallet address
+amount = 500 ##amount balance you want withdraw
